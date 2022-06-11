e5b84dee6d2ad55e06b5725b11baf3c01e44ac83
Ivo <ivoexesosa88@gmail.com>
2022-06-11 02:34:12 -0300

    Agregado ejercicio2

---
+++ src/ejercicio2.py
@@ -0,0 +1,35 @@
+################
+# Nombre - @IvoEST88
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+def min_max_promedio(secuencia):
+    long_secuencia = len(secuencia)
+    mayor = secuencia[0]
+    menor = secuencia[0]
+    promedio = 0
+    vueltas = 0
+    while vueltas < long_secuencia:
+        promedio += secuencia[vueltas]
+        if secuencia[vueltas] < menor:
+            menor = secuencia[vueltas]
+        if secuencia[vueltas] > mayor:
+            mayor = secuencia[vueltas]
+        vueltas += 1
+    promedio = (promedio // long_secuencia)
+    lista = [menor, mayor, promedio]
+    tupla = tuple(lista)
+    return tuple
+
+
+def principal():
+    limite_secuencia = int(input("¿Cuántos números desea ingresar?: "))
+    secuencia = []
+    print("Ingrese: ")
+    while limite_secuencia > 0:
+        numero = int(input())
+        secuencia.append(numero)
+        limite_secuencia -= 1
+    print (min_max_promedio(secuencia))
+if __name__ == "__main__":
+    principal()
