3c1b1e4af8ed645733c22a6334d3d59f3c217a14
Ivo <ivoexesosa88@gmail.com>
2022-06-18 09:44:54 -0300

    Errores encontrados gracias al test y solucionados

--- src/ejercicio3.py
+++ src/ejercicio3.py
@@ -12,25 +12,19 @@ def superposicion(frase1, frase2):
     Funcion que indica cuales letras se repiten en ambas frases
     """
     vueltas = 0
-    vueltas2 = 0
     long_frase1 = len(frase1)
     long_frase2 = len(frase2)
     en_comun = 0
     if long_frase1 > long_frase2:
-        frase_larga = long_frase1
-    elif long_frase1 < long_frase2:
-        frase_larga = long_frase2
+        frase_corta = long_frase2
     else:
-        frase_larga = long_frase1
-    while vueltas < frase_larga:
-        while vueltas2 < long_frase2:
-            vueltas1 = 0
-            while vueltas1 < long_frase1:
-                if frase1[vueltas1] == frase2[vueltas2]:
-                    en_comun += 1
-                vueltas1 += 1
-            vueltas2 += 1
-        vueltas += 1
+        frase_corta = long_frase1
+    while vueltas < frase_corta:
+        if frase1[vueltas] == frase2[vueltas]:
+            en_comun += 1
+            vueltas += 1
+        else:
+            vueltas += 1
     return en_comun
 
 
